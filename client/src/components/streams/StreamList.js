d49cbbb73701cbb8bac7bbfcd2c0a5184a874c34
github username <github email address>
2018-12-29 11:33:39 -0800

    add comments to StreamList.js

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,13 +1,25 @@
+//Start imports
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStreams } from "../../actions";
 import { Link } from "react-router-dom";
 
+/**
+ * Streamlist is a class-based component that fetches and displays the list of our streams
+ **/
 class StreamList extends React.Component {
+  /**
+   * componentDidMount calls the fetchStreams action to retreive a list of Streams
+   * from our rails api backend
+   **/
   componentDidMount() {
     this.props.fetchStreams();
   }
 
+  /**
+   * renderAdmin renders administrative buttons on a given stream if the current user
+   * is the user that created the stream
+   **/
   renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
       return (
@@ -26,6 +38,10 @@ class StreamList extends React.Component {
     }
   }
 
+  /**
+   * renderList loops over the streams object in this components props and returns
+   * a list of the streams and their properties to be displayed
+   **/
   renderList() {
     return this.props.streams.map(stream => {
       return (
@@ -43,6 +59,9 @@ class StreamList extends React.Component {
     });
   }
 
+  /**
+   * renderCreate returns the create stream button
+   **/
   renderCreate() {
     if (this.props.isSignedIn) {
       return (
@@ -55,6 +74,9 @@ class StreamList extends React.Component {
     }
   }
 
+  /**
+   * render renders our list of streams and the create stream button on the page
+   **/
   render() {
     return (
       <div>
@@ -66,6 +88,9 @@ class StreamList extends React.Component {
   }
 }
 
+/**
+ * mapStateToProps maps the streams, currentUserId, and isSignedIn values to this component's props
+ **/
 const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
@@ -74,6 +99,9 @@ const mapStateToProps = state => {
   };
 };
 
+/**
+ * export StreamList and hook it up to our redux store with mapStateToProps and the fetchStreams action
+ **/
 export default connect(
   mapStateToProps,
   { fetchStreams }
