7ce3de56ff7463b24ab0ba35c2dc5ef6d7f43c2d
github username <github email address>
2018-12-29 11:54:19 -0800

    add comments for StreamShow.js

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,15 +1,27 @@
+//Start imports
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 import flv from "flv.js";
 
+/**
+ * StreamShow is a class-based compnent that displays a stream to our user
+ **/
 class StreamShow extends React.Component {
+  /**
+   * constructor initiates our component and sets a videoRef variable for our
+   * video object
+   **/
   constructor(props) {
     super(props);
 
     this.videoRef = React.createRef();
   }
 
+  /**
+   * componentDidMount fetches our stream object when the component first mounts
+   * as well as builds our video player
+   **/
   componentDidMount() {
     const { id } = this.props.match.params;
 
@@ -17,14 +29,23 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  /**
+   * componentDidUpdate rebuilds our video player every time the component re-renders
+   **/
   componentDidUpdate() {
     this.buildPlayer();
   }
 
+  /**
+   * componentWillUnmount destroys our video player as our component unmounts
+   **/
   componentWillUnmount() {
     this.player.destroy();
   }
 
+  /**
+   * buildPlayer creates, attaches and loads our live-stream from our streaming server
+   **/
   buildPlayer() {
     const { id } = this.props.match.params;
 
@@ -40,6 +61,9 @@ class StreamShow extends React.Component {
     this.player.load();
   }
 
+  /**
+   * render renders a single stream onto the webpage, with the video player
+   **/
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
@@ -57,10 +81,17 @@ class StreamShow extends React.Component {
   }
 }
 
+/**
+ * mapStateToProps returns our stream for access through props
+ **/
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
+/**
+ * export the component and connect it to our redux store, passing it our state as props
+ * and the fetchStream action
+ **/
 export default connect(
   mapStateToProps,
   { fetchStream }
