0e73f674a1ddad0c01a5304110d367e9b09ba847
github username <github email address>
2018-12-29 12:15:46 -0800

    add comments to reducers index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,13 @@
+//Start imports
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 import streamReducer from "./streamReducer";
 
+/**
+ * export combineReducers which takes all of our reducers and combines them into
+ * a single object
+ **/
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
