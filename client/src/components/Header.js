82f2285753f9727b2e0b644b5840127c6aa6f898
github username <github email address>
2018-12-29 12:08:28 -0800

    add comments to Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,11 @@
+//Start imports
 import React from "react";
 import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
 
+/**
+ * Header is a functional component that returns the display of our App's header
+ **/
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
@@ -18,4 +22,7 @@ const Header = () => {
   );
 };
 
+/**
+ * export our Header component
+ **/
 export default Header;
