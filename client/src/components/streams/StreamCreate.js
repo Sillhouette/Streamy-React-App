67f499b359412f51be647e13da3d700d885901b1
github username <github email address>
2018-12-28 20:34:00 -0800

    add comments to components/streams/createStream.js

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,13 +1,25 @@
+//Start imports
 import React from "react";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
 import StreamForm from "./StreamForm";
 
+/**
+ * StreamCreate is a class-based component that displays a StreamForm which is
+ * used to get the data required to create a new stream
+ **/
 class StreamCreate extends React.Component {
+  /**
+   * onSubmit takes in the submitted form values and passes them to the
+   * createStream action
+   **/
   onSubmit = formValues => {
     this.props.createStream(formValues);
   };
 
+  /**
+   * Render function tells React how we want to render our stream creation component
+   **/
   render() {
     return (
       <div>
@@ -18,6 +30,10 @@ class StreamCreate extends React.Component {
   }
 }
 
+/**
+ * Export this component while passing it to Redux's connect component wiring this
+ * component to redux through the createStream action
+ **/
 export default connect(
   null,
   { createStream }
