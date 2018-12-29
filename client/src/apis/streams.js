07b24c117fc2989dfa0476fdd5e5b753f33d4db9
github username <github email address>
2018-12-28 20:27:07 -0800

    add comments to apis/stream.js

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -1,5 +1,9 @@
+//Start imports
 import axios from "axios";
 
+/**
+ * Create an instance of axios to interact with our rails backend
+ **/
 export default axios.create({
   baseURL: "http://localhost:3000/api/"
 });
