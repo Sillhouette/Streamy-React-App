0128476d61abe6beca43f468e0015b586c95d323
github username <raustin.melchior@gmail.com>
2019-09-18 08:57:16 -0700

    hookup api attempt 2

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -5,5 +5,5 @@ import axios from "axios";
  * Create an instance of axios to interact with our rails backend
  **/
 export default axios.create({
-  baseURL: "http://localhost:3000/api/"
+  baseURL: "http://www.stream-source.net/api/v1/"
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Our stream resources should only be accessed through '/api' urls
-  scope '/api' do
-    resources :streams
+  namespace :api do
+    namespace :v1 do
+      resources :streams
+    end
+end
+  # app.get('*', (req, res) => {
+  #   res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  # });
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
