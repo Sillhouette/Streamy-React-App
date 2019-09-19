08f3003236dda56712ad26966a249bcb58c4bfcd
github username <raustin.melchior@gmail.com>
2019-09-18 18:30:42 -0700

    fix routing issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
   include ActionController::MimeResponds
 
   def fallback_index_html
