aa636437f03c215cb7ac59598d3e77af681414ee
github username <raustin.melchior@gmail.com>
2019-09-18 18:26:09 -0700

    add mimeresponds to applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
-  include Response
-  include ExceptionHandler
+  include ActionController::MimeResponds
+
   def fallback_index_html
     respond_to do |format|
       format.html { render body: Rails.root.join('public/index.html').read }
