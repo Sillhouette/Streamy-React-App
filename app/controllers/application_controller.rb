e24850e14c3474cfb7627c9b1a842c969106796a
github username <raustin.melchior@gmail.com>
2019-09-18 18:08:46 -0700

    add handling for router to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+  def fallback_index_html
+    respond_to do |format|
+      format.html { render body: Rails.root.join('public/index.html').read }
+    end
+  end
 end
