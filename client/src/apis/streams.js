f03bdb10d16de19696d0afe05deb477a55547f04
github username <raustin.melchior@gmail.com>
2019-10-01 15:25:29 -0700

    add ip

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -5,5 +5,5 @@ import axios from "axios";
  * Create an instance of axios to interact with our rails backend
  **/
 export default axios.create({
-  baseURL: "http://localhost:3000/api/v1/"
+  baseURL: "http://134.209.8.173:3000/api/v1/"
 });
