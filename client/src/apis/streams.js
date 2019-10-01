98e821a3f5666573f51f75c20bf5d2703eac475d
github username <raustin.melchior@gmail.com>
2019-10-01 15:07:57 -0700

    change config

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -5,5 +5,5 @@ import axios from "axios";
  * Create an instance of axios to interact with our rails backend
  **/
 export default axios.create({
-  baseURL: "http://www.stream-source.net/api/v1/"
+  baseURL: "http://localhost:3000/api/v1/"
 });
