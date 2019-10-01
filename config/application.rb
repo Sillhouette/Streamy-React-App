68ca51794f1ff6ff6590ea13226755dc7be4744e
github username <raustin.melchior@gmail.com>
2019-10-01 15:32:28 -0700

    fix cors issue

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ module NewsHub
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins 'http://134.209.8.173'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
