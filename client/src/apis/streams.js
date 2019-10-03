928b64352b518f88f7a7df10e14616d6c573429b
github username <raustin.melchior@gmail.com>
2019-10-03 12:29:12 -0700

    change back to http

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -5,5 +5,5 @@ import axios from "axios";
  * Create an instance of axios to interact with our rails backend
  **/
 export default axios.create({
-  baseURL: "https://api.stream-source.net/api/v1/"
+  baseURL: "http://api.stream-source.net/api/v1/"
 });
